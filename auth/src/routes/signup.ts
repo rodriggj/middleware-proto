c52f837368584d3c1f79a68ffe3ce9076a7bfd52
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2024-11-02 17:22:48 -0600

    adding 2 error subclasses to our errorhandling middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import { body, validationResult } from 'express-validator'
 import { Request, Response } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
+
 
 const router = express.Router()
 
@@ -12,18 +15,14 @@ router.post("/api/users/signup",
         .isLength({ min: 4, max: 20 })
         .withMessage("Password must be between 4 and 20 characters"),
     ], (req: Request, res: Response): any => {
-
         const errors = validationResult(req)
 
         if(!errors.isEmpty()) {
-            // return res.status(400).send(errors.array())
-            throw new Error(`Invalid email or password.`)
+            throw new RequestValidationError(errors.array())
         }
-        
-        const { email, password } = req.body
 
         console.log('Creating a new user...')
-        throw new Error('Error connecting to the database.')
+        throw new DatabaseConnectionError()
 
         res.send({})
     }
