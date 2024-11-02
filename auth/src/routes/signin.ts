bf220de25ac0ceac0c94008ae02fb72d6f0f2027
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2024-11-02 08:59:51 -0600

    modifying boilerplate routes with appropriate http method

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,8 +2,8 @@ import express from 'express'
 
 const router = express.Router()
 
-router.get('/api/users/signin', () => {
-
+router.post('/api/users/signin', (req, res) => {
+    res.send('You have created a successful post to Signin.')
 })
 
 export { router as signInRouter }
