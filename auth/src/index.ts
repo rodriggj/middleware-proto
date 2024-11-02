8d70f3f96bedfc5fac6112438e03d293ad45ae32
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2024-11-02 08:49:28 -0600

    adding route handlers for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,16 +1,22 @@
 import express from 'express'
 import { json } from 'body-parser'
 
+import { currentUserRouter } from './routes/current-user'
+import { signInRouter } from './routes/signin'
+import { signUpRouter } from './routes/signup'
+import { signOutRouter } from './routes/signout'
+
 const app = express()
 app.use(json())
 
 const SERVICE = "Auth"
 const PORT = process.env.PORT || 3000
 
-app.get("/api/users/currentuser", (req, res) => {
-    res.send(`Hi There!`)
-    }
-)
+// Auth Services Routes
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signUpRouter)
+app.use(signOutRouter)
 
 app.listen( 3000, () => {
     console.log(`Your ${SERVICE} service is up and listening on port ${PORT}`
