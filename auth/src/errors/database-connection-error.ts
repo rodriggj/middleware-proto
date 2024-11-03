a497ec597ec0713757eaaa1d08de1f8418a35042
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2024-11-02 22:20:27 -0600

    add serialize errors logic for common error object response

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,7 +1,7 @@
 import { ValidationError } from 'express-validator'
 
 export class DatabaseConnectionError extends Error {
-
+    statusCode = 500
     reason = "Error Connecting to Database."
     
     constructor (){
@@ -10,4 +10,11 @@ export class DatabaseConnectionError extends Error {
         // Only because we are extending a built in Class (errors) -- typescript requirement
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
     }
+
+    serializeErrors(){
+        return [
+            { message: this.reason }
+        ]
+    }
+
 }
