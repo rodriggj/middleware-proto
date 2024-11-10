ed9c55ab6751cf39cc10874e8e2f404dafff3624
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2024-11-10 15:25:08 -0700

    configure cookie-sessions

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
 
 // Import Auth Routers 
 import { currentUserRouter } from './routes/current-user'
@@ -12,7 +13,12 @@ import { errorHandler } from './middleware/error-handler'
 import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
+app.set('trust proxy', true)   // lets express know that traffic is being proxied by ingress-nginx and the traffic is secure
 app.use(json())
+app.use(cookieSession({
+  signed: false,   // doesn't need to be encrypted b/c JWT already does this
+  secure: true   // require https connection
+}))
 
 const SERVICE = "Auth"
 const PORT = process.env.PORT || 3000
