f10e53a344a7789da8ada7f8d79bc0feea329fb2
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2024-11-11 06:34:15 -0700

    added initial signin config for req/res

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,27 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
+
+import { RequestValidationError } from '../errors/request-validation-error'
 
 const router = express.Router()
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('You have created a successful post to Signin.')
+router.post('/api/users/signin', 
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('You must supply a password')
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array())
+        }
+        res.status(200).json({ msg: 'You are signed in.' })
 })
 
 export { router as signInRouter }
