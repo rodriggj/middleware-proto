9b1b84796dccea315e933483ed4a268d48fd6e0f
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2024-11-24 14:39:56 -0700

    created current-user middleware and implemented

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,23 +1,11 @@
 import express, {Request, Response} from 'express';
 import jwt from 'jsonwebtoken';
+import { currentUser } from '../middleware/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res:Response):any => {
-  //first check to see if there is a JWT associated with Session.
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-  // next check to see if the JWT has been temperated with ... it should contain the session JWT that we created as Signup. 
-  try {
-    const payload = jwt.verify(
-        req.session.jwt, 
-        process.env.JWT_KEY!
-    );
-    res.send({ currentUser: payload });   // if not tampered ... send the current user payload
-  } catch (err) {
-    res.send({ currentUser: null });  // if no JWT or not valid, send a "null"
-  }
+router.get('/api/users/currentuser', currentUser, (req: Request, res:Response):any => {
+  res.send( { currentUser: req.currentUser || null })
 });
 
 export { router as currentUserRouter };
