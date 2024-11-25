3717118643030b099fa9b39044be8f9944a77979
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2024-11-24 20:13:46 -0700

    adding test/setup.ts

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,31 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+import { app } from '../app'   // our express app that we moved from index to app file
+
+let mongo: any
+
+// Before we start our tests, we need to create an instance of MongoMemoryServer in memory, to do this we will 
+// define a hook function, and make it async. This functional will run "beforeAll" subsequent tasks in our function
+beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri();
+
+    await mongoose.connect(mongoUri, {});
+})
+
+beforeEach(async () => {
+    if (mongoose.connection.db) {
+      const collections = await mongoose.connection.db.collections();
+   
+      for (let collection of collections) {
+        await collection.deleteMany({});
+      }
+    }
+  });
+
+  afterAll(async () => {
+    if (mongo) {
+      await mongo.stop();
+    }
+    await mongoose.connection.close();
+  });
