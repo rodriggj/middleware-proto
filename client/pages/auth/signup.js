64155281d593b49dd7b403fa6669acba05d2ad31
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2024-11-30 12:34:41 -0700

    added useRequest hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,44 +1,47 @@
-import { useState } from 'react'
-import axios  from 'axios'
+import { useState } from 'react';
+import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [errors, setErrors] = useState([])
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password
+    }
+  });
 
-    const onSubmit = async (event) => {
-        event.preventDefault()
+  const onSubmit = async event => {
+    event.preventDefault();
 
-        try {
-            // console.log(`Form submitted with email: ${email} and password ${password}`)
-            const response  = await axios.post('/api/users/signup', {
-                email,
-                password
-        })
-        console.log(response.data)
-        } catch (err) {
-            // console.log(err.response.data)
-            setErrors(err.response.data.errors)
-        }
-    } 
-    return <form onSubmit={onSubmit}>
-        <h1>Signup</h1>
-        <div className="form-group">
-            <label>Email Address</label>
-            <input value={ email } onChange={ e => setEmail( e.target.value ) }className="form-control"></input>
-        </div>
-        <div className="form-group">
-            <label>Password</label>
-            <input value={ password } onChange={ e => setPassword( e.target.value ) }type="password" className="form-control"></input>
-        </div>
-        {errors.length > 0 && (<div className="alert alert-danger">
-            <h4>Oooops...</h4>
-            <ul className="my-0">
-                {errors.map(err => (
-                    <li key={err.message}>{err.message}</li> 
-                ))}
-            </ul>
-        </div>)}
-        <button className="btn btn-primary">Sign Up</button>
+    doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign Up</button>
     </form>
-}
+  );
+};
